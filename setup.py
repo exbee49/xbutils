8e1595373f280a1b98b364bdbdd77a73aa2b6200
XB49 <exbee49@free.fr>
2022-09-05 16:11:03 +0200

    v0.0.3

--- setup.py
+++ setup.py
@@ -2,11 +2,19 @@ from setuptools import setup
 
 setup(
     name='xbutils',
-    version='0.0.1',
+    version='0.0.3',
     packages=['xbutils'],
-    url='',
+    url='https://github.com/exbee49/xbutils',
     license='WTFPL',
     author='xb49',
     author_email='',
-    description=''
+    description='Python utilities',
+    long_description="Some Python utility classes",
+    long_description_content_type="text/plain",
+
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        # "license :: Public Domain :: WTFPLv2",
+        "Programming Language :: Python :: 3 :: Only"
+    ]
 )
